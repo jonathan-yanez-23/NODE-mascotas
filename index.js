046359d2f7f0f29db221f1630739219298f1e7de
jonathan yanez <jyv666@gmail.com>
2021-11-02 01:38:49 +0100

    Problemas con el hbs: carpeta public, app.use, gte, etc

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("./db");
 // Modulos que hacen falta
 const express = require("express");
 const Pet = require("./models/Pet");
+const path = require("path");
+const hbs = require("hbs");
 
 // CONFIGURAR SERVER
 const server = express();
@@ -14,10 +16,13 @@ const PORT = 4044;
 // RUTAS
 petRoutes = require("./routes/pet.routes");
 shelterRoutes = require("./routes/shelter.routes");
+indexRoutes = require("./routes/index.routes");
 
 // AGREGAR Routes
 server.use("/pets", petRoutes);
 server.use("/shelters", shelterRoutes);
+server.use("/", indexRoutes);
+
 
 // Middleware para cuando no haya ruta
 server.use("*", (req, res, next) => {
@@ -26,12 +31,31 @@ server.use("*", (req, res, next) => {
     next(error);
 });
 
-// Agregar el controlador de errores.
+// Agregar controlador de errores con vista de ERROR
 server.use((err, req, res, next) => {
-    return res.status(err.status || 500).json(err.message || "Unexpected error");
+    return res.status(err.status || 500).render('error', {
+        message: err.message || 'Unexpected error',
+        status: err.status || 500,
+    });
 });
 
+server.use(express.static(path.join(__dirname, "public")));
 
+// Handlebars
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "hbs");
+//Agregar el helper custom (if customizado)
+hbs.registerHelper("gte", (a, b, opts) => {
+    if (a >= b){
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
+hbs.registerHelper("uppercase", (str) => {
+    return str.toUpperCase();
+});
 server.listen(PORT, ()=>{
     console.log(`server running on localhost:${PORT}`);
 });
---
+++ routes/index.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const Pet = require("../models/Pet");
+router.get("/", async (req, res, next)=>{
+    try {
+        const pets = await Pet.find();
+        res.status(200).render("index", {title: "Upgrade Pets", pets: pets});
+    } catch (err) {
+        next(err);
+    }
+    
+});
+
+router.get("/pet/:id", async (req, res, next)=>{
+    try {
+        const id  = req.params.id;
+        const pet = await Pet.findById(id);
+        return res.status(200).render("pet",{
+            title: "Upgrade single pet",
+            pet: pet,
+            id: id
+        });
+    } catch (err) {
+        // return res.status(404).render("pet",{
+        //     title: "Upgrade single pet",
+        //     pet: undefined,
+        //     id: req.params.id
+        // });
+        next(err);
+    }
+    
+});
+module.exports = router;
